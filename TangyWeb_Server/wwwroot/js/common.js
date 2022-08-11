336f056dff6d93c412d34ed1e445471eeced1214
victornjg <victor.nnjg@gmail.com>
2022-08-11 20:40:14 -0300

    Section 6 - Toastr and SweetAlert integrations.

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,27 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
+
+window.ShowSweetAlert = (type, message) => {
+    if (type === "success") {
+        Swal.fire({
+            title: "Operation Successful",
+            text: message,
+            icon: "success",
+            confirmButtonText: "OK"
+        })
+    }
+    if (type === "error") {
+        Swal.fire({
+            title: "Operation Failed",
+            text: message,
+            icon: "error",
+            confirmButtonText: "OK"
+        });
+    }
+}
