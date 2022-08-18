f10494936a8699f2e96f69a3de0e1c5613841fbf
victornjg <victor.nnjg@gmail.com>
2022-08-18 13:00:11 -0300

    Section 13

---
+++ TangyWeb_API/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tangy_Business.Repository.IRepository;
+using Tangy_Models;
+
+namespace TangyWeb_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepo;
+
+        public ProductController(IProductRepository productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _productRepo.GetAll());
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> Get(long? productId)
+        {
+            if (productId == null || productId <= 0)
+            {
+                return BadRequest(new ErrorModelDTO()
+                {
+                    ErrorMessage = "Invalid Id",
+                    StatusCode = StatusCodes.Status400BadRequest
+                });
+            }
+
+            var product = await _productRepo.Get((long)productId);
+            if (product.Id <= 0)
+            {
+                return BadRequest(new ErrorModelDTO()
+                {
+                    ErrorMessage = "Product not found",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+
+            return Ok(product);
+        }
+    }
+}
